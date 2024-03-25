22f076c55ee22621c5262259bbff741ca6b13a9d
Konstantin M <htmlmak@gmail.com>
2024-03-26 00:51:43 +0700

    Add files

--- lecture-4/server/config.py
+++ lecture-4/server/config.py
@@ -4,5 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', "fdsgkdf1gj")
+    PORT = os.environ.get('PORT', 5000)
     DEBUG_TB_PROFILER_ENABLED = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.sqlite3')
